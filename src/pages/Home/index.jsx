80c45bc2c92f3deb385a547c004cdab5ff26060e
Amanda Guerra <guerraamanda1@hotmail.com>
2022-09-14 16:51:40 -0300

    Primeiro commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+import * as C from "./styles"
+
+export function Home() {
+  return (
+  <C.Container>
+    <h1>Hello</h1>
+  </C.Container>
+  )
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,17 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#000A0F",
+    BACKGROUND_800: "#00111A",
+    BACKGROUND_700: "#0D1D25",
+    WHITE: "#FFFFFF",
+
+    GRAY_100: "#E1E1E6",
+    GRAY_200: "#C4C4C4",
+
+    PRIMARY: "#82F3FF",
+
+    RED: "#92000E",
+    ORANGE: "#FBA94C",
+    GREEN: "#04D361" 
+  }
+}
