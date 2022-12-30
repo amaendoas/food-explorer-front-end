a8a2c7ac9a4cb7e9e38e0e3812012dc1720120e3
Amanda Guerra <guerraamanda1@hotmail.com>
2022-12-30 11:17:38 -0300

    ajustes de responsividade pagina detalhes

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -4,14 +4,14 @@ export const Container = styled.div`
   position: relative;
   display: grid;
   width: 100%;
-  height: 90%;
-  grid-template-columns: 40% 60%;
+  height: 80%;
+  grid-template-columns: 50% 50%;
   grid-template-areas: "img info";
   background-color: ${({theme}) => theme.COLORS.BACKGROUND_900};
   -webkit-box-shadow: 0px 0px 40px 4px rgba(0, 0, 0, 0.56);
   -moz-box-shadow: 0px 0px 40px 4px rgba(0, 0, 0, 0.56);
   box-shadow: 0px 0px 40px 4px rgba(0, 0, 0, 0.56);
-  border-radius: 5px;
+  border-radius: 10px;
 `
 
 export const DishImg = styled.div`
@@ -22,7 +22,7 @@ export const DishImg = styled.div`
   justify-content: center;
 
   img {
-    width: calc(8rem + 10vw);
+    width: calc(8rem + 12vw);
     height: auto;
     border-radius: 50%;
   }
