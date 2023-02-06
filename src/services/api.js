aa7ae1ac4832f40a0666acca8da39e9c91ff70fd
Amanda Guerra <guerraamanda1@hotmail.com>
2023-02-06 16:17:58 -0300

    reiniciar

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "http://localhost:3333"
+  // baseURL: "http://localhost:3334"
   baseURL: "https://food-explorer-9uuy.onrender.com"
   
 })
